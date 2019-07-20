e9ac86f393d90200610284074d790d0f72b52c9a
hassan177 <daniyalnagori@yahoo.com>
2019-07-20 19:50:18 +0500

    API Key update

--- src/app/services/whether.service.ts
+++ src/app/services/whether.service.ts
@@ -9,15 +9,15 @@ export class WhetherService {
   urlLocations: string = 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete'
   urlCurrentCondition: string = 'https://dataservice.accuweather.com/currentconditions/v1'
   url5DayForecast: string = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/'
-  apiKey: string = 'kxTOlJKmtuoQ1myMTxpKECq1GYA6sUdM&q'
+  apiKey: string = 'ewEK4IDkvj5AXAdzTRQspDijWAANl1IR'
 
   constructor(private http: Http) { }
 
   search(query: string) {
     if (query === ' ' || query === '') {
-      return this.http.get(`${this.urlLocations}?apikey=${this.apiKey}=Tel Aviv`).pipe(map(res => res.json()));
+      return this.http.get(`${this.urlLocations}?apikey=${this.apiKey}&q=Tel Aviv`).pipe(map(res => res.json()));
     }
-    return this.http.get(`${this.urlLocations}?apikey=${this.apiKey}=${query}`).pipe(map(res => res.json()));
+    return this.http.get(`${this.urlLocations}?apikey=${this.apiKey}&q=${query}`).pipe(map(res => res.json()));
   }
 
   getCurrentWhether(locationKey: number) {
